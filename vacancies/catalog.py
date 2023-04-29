d47c5eed69e656fc4b888cad4d295dd78af8d07c
Andrey <hamanist@gmail.com>
2023-04-29 22:52:04 +0300

    added a class for working with files

---
+++ vacancies/catalog.py
@@ -0,0 +1,18 @@
+from typing import List
+
+from parser.parser_sj import Parser_SJ
+from vacancies.sj import Vacancies_SJ
+
+
+class Catalog:
+    list_sj: List[Vacancies_SJ] = []
+    list_hh: List = []
+
+    def upload_vacancies(self):
+        self.list_sj = Parser_SJ().get_vacancies()
+        # self.list_hh = Parser_HH().get_vacancies()
+
+    def save_to_file(self):
+        if self.list_sj or self.list_hh:
+            with open('./vacancies.csv', 'a', encoding='utf-8') as file:
+                file.writelines([report.get_from_string() for report in self.list_sj + self.list_hh])
